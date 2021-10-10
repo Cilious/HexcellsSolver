ffaee27cbb9dbfaec986a0bd01faa0a5b2fe1704
Joshua <joshua.ocker@gmail.com>
2021-10-10 17:43:18 +0200

    Add error message in main

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from level_parser import parse, HexcellsLogoNotFoundException
 
 
 try:
-    print(parse("test2.png"))
+    print(parse())
 except HexcellsLogoNotFoundException:
     print("Hexcells Logo was not found in the taskbar. Try:")
     print("     1. Ensure Hexcells is open")
     print("     2. Take a screenshot of the logo in the taskbar and add it to the directory hexcells_logo")
-
+except IndexError:
+    print("Something went wrong. Please try again.")
+    print("If the issue keeps persisting, please contact the project creator with the seed of level.")
